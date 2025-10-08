b987edc303ff5dee90cb122b577f898a48dbdd33
marcosmercedesn <marcos.mercedesn@gmail.com>
2025-10-08 09:30:18 -0400

    Adapt to new ransack requirements

--- spec/test_app/app/models/application_record.rb
+++ spec/test_app/app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.ransackable_associations(*, **) = reflections.keys
+  def self.ransackable_attributes(*, **) = attribute_names
 end
